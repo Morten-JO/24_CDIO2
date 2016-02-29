173a4c8a898008d134879c2a43998a12ae74d84f
Mortenbaws <morten2094@hotmail.com>
2016-02-29 15:33:53 +0100

    base

---
+++ src/test.java
@@ -0,0 +1,18 @@
+
+/**
+ * Date: 29/02/2016
+ *
+ * Project: 24_CDIO2
+ *
+ * File: test.java
+ *
+ * Created by: Morten Jørvad
+ *
+ * Github: https://github.com/Mortenbaws
+ *
+ * Email: morten2094@gmail.com
+ */
+
+public class test {
+
+}
